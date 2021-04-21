4202dcb61a8fe0decd3f5d0a0dc5d94a6a6e84ea
Matheus Henrique <matherique@gmail.com>
2021-04-20 21:25:45 -0300

    add helpers functions to handle corret status response

--- routes/utils.go
+++ routes/utils.go
@@ -1,7 +1,37 @@
 package routes
 
+import (
+	"encoding/json"
+	"net/http"
+)
+
 const (
 	MethodGET  = "GET"
 	MethodPOST = "POST"
 	MethodPUT  = "PUT"
 )
+
+func created(w http.ResponseWriter, r *http.Request, response interface{}) {
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(response)
+}
+
+func ok(w http.ResponseWriter, r *http.Request, response interface{}) {
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(response)
+}
+
+func notFound(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusNotFound)
+	w.Write([]byte("not found"))
+}
+
+func internalServerError(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusInternalServerError)
+	w.Write([]byte("internal server error"))
+}
+
+func badRequest(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusBadRequest)
+	w.Write([]byte("bad request"))
+}
