2a53628c2cd4a478f9479bf20b8dcf37d1f90883
Matheus Henrique <matherique@gmail.com>
2021-04-20 21:55:30 -0300

    handle invalid url

--- routes/routes.go
+++ routes/routes.go
@@ -2,15 +2,25 @@ package routes
 
 import (
 	"net/http"
+	"regexp"
 )
 
 type homeRoutes struct{}
 
+var homePattern = regexp.MustCompile(`^\/$`)
+
 func (h homeRoutes) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("GET /home"))
+	switch true {
+	case r.Method == MethodGET && homePattern.MatchString(r.URL.Path):
+		w.Write([]byte("GET /home"))
+		return
+	default:
+		notFound(w, r)
+	}
 }
 
 func LoadRoutes(server *http.ServeMux) {
 	server.Handle("/", homeRoutes{})
 	server.Handle("/users", usersRoute{})
+	server.Handle("/users/", usersRoute{})
 }
