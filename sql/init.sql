4e301bf403488703e5a6b83c8f6af6900540e744
Matheus Henrique <matherique@gmail.com>
2021-12-04 20:19:05 -0300

    clean the repo

--- sql/init.sql
+++
@@ -1,7 +0,0 @@
-create table if not exists users (
-  id serial not null primary key,
-  name varchar(100) not null,
-  email varchar(50) not null, 
-  createdAt timestamp not null default CURRENT_TIMESTAMP,
-  updatedAT timestamp not null default CURRENT_TIMESTAMP
-)
